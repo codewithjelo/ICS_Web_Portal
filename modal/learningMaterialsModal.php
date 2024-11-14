d6cd8b0689ef82dbbce1e44f8114aca8b3a7220e
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-11-14 23:39:29 +0800

    Fix post and uploaded materials

--- modal/learningMaterialsModal.php
+++ modal/learningMaterialsModal.php
@@ -1,28 +1,12 @@
 <!-- Modal -->
 <?php
 include '../connectDb.php';
-
-// Fetch uploaded materials for the "Uploaded" tab
-$query = "SELECT * FROM school_materials";
-$result = $conn->query($query);
-$uploaded_files = []; // Initialize the variable as an empty array
-
-// Check if the query executed successfully and returned results
-if ($result && $result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $uploaded_files[] = $row; // Add each row to the uploaded_files array
-    }
-} else {
-    // If no files are found, $uploaded_files will remain empty
-    // You can set an empty array as default or handle this case differently
-    $uploaded_files = [];
-}
 ?>
 
 
 <link rel="stylesheet" href="../css/modal.css">
-<script src="../js/gradeLevelMaterials.js"></script>
-<script src="..js/getUploadedMaterials.js"></script>
+<script src="../js/getSectionMaterials.js"></script>
+<script src="../js/filterSectionMaterials.js"></script>
 
 <div class="modal fade modal-xl" id="teacherMaterialsModal" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -32,34 +16,39 @@ if ($result && $result->num_rows > 0) {
                 <button type="button" class="btn-close position-absolute top-0 end-0" style="top: 25px !important; right: 25px !important;" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-          
+
+
                 <ul class="nav nav-tabs mx-3" id="uploadTabs" role="tablist">
+
                     <li class="nav-item" role="presentation">
                         <button class="nav-link active" id="post-tab" data-bs-toggle="tab" data-bs-target="#post" type="button" role="tab" aria-controls="post" aria-selected="true">Post</button>
                     </li>
+
                     <li class="nav-item" role="presentation">
                         <button class="nav-link" id="uploaded-tab" data-bs-toggle="tab" data-bs-target="#uploaded" type="button" role="tab" aria-controls="uploaded" aria-selected="false">Uploaded</button>
                     </li>
+
                 </ul>
+
 
                 <div class="tab-content" id="postTabContent">
 
+
                     <div class="tab-pane fade show active" id="post" role="tabpanel" aria-labelledby="post-tab" style="height: 400px;">
+
                         <!-- Upload Section -->
                         <div class="card m-3">
-                            <div class="card-body">
+
+
+                            <div class="card-body" style="height: 358px;">
+
                                 <h5 class="card-title">Post</h5>
+
                                 <form action="../function/uploadSchoolMaterial.php" id="uploadForm" method="POST" enctype="multipart/form-data">
-                                    <div class="mb-3">
-                                        <label for="gradeLevel" class="form-label">Grade Level</label>
-                                        <select id="gradeLevel" class="form-select" name="grade">
-                                            <option selected disabled>Grade Level</option>
-                                        </select>
-                                    </div>
 
                                     <div class="mb-3">
-                                        <label for="sectionLevel" class="form-label">Section</label>
-                                        <select id="sectionLevel" class="form-select" name="section_name">
+                                        <label for="sectionMaterials" class="form-label">Section</label>
+                                        <select id="sectionMaterials" class="form-select" name="section_name">
                                             <option selected disabled>Section</option>
                                         </select>
                                     </div>
@@ -67,17 +56,28 @@ if ($result && $result->num_rows > 0) {
                                     <div class="mb-3">
                                         <label for="fileInput" class="form-label">Upload File</label>
                                         <input type="file" class="form-control" id="fileInput" name="school_materials">
+
                                     </div>
+
                                     <button type="submit" class="btn btn-primary">Post</button>
+
                                 </form>
+
                             </div>
+
+
                         </div>
+
+
                     </div>
+
 
                     <div class="tab-pane fade" id="uploaded" role="tabpanel" aria-labelledby="uploaded-tab" style="height: 400px;">
                         <!-- Uploaded Section -->
                         <div class="card m-3 overflow-y-auto" style="height: 360px;">
+
                             <div class="card-body">
+
                                 <div class="row position-relative">
                                     <div class="col-md-8">
                                         <h5 class="card-title">Uploaded</h5>
@@ -90,27 +90,18 @@ if ($result && $result->num_rows > 0) {
                                         </div>
                                     </div>
                                 </div>
+
+
                                 <div class="overflow-y" id="uploadedFiles" style="max-height: 295px;">
-                                    <?php foreach ($uploaded_files as $file): ?>
-                                    <div class="row column-gap-5 mt-3 mx-1 align-content-center position-relative rounded-3 border border-1" style="height: 40px;">
-                                        <div class="col-md-8 ms-2">
-                                            <?php echo basename($file['school_materials']); ?>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <a href="<?php echo $file['school_materials']; ?>" class="btn btn-secondary position-absolute top-50 end-0 translate-middle-y border border-0 me-5" style="height: 40px; width: 50px; background-color: transparent;" download>
-                                                <i class="bi bi-download" style="color: black;"></i>
-                                            </a>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <button class="btn btn-secondary position-absolute top-50 end-0 translate-middle-y border border-0" style="height: 40px; width: 50px; background-color: transparent;">
-                                                <i class="bi bi-trash" style="color: black;"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <?php endforeach; ?>
+
+                                    <!-- List of Uploaded Materials by Section -->
+                                    <p class="text-center pt-5" style="color: gray;">Select a section.</p>;
                                 </div>
+
                             </div>
+
                         </div>
+
                     </div>
                 </div>
             </div>
