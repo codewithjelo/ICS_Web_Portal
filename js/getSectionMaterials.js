0ff49796a398ecd18806ad388bb76d27e8d81912
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-11-14 23:38:45 +0800

    Create ajax for get section materials

---
+++ js/getSectionMaterials.js
@@ -0,0 +1,27 @@
+function loadSections() {
+    $.ajax({
+        url: '../function/fetchSectionMaterials.php', // Path to the external PHP file
+        type: 'GET',
+        success: function(response) {
+            const sections = JSON.parse(response);
+            const sectionSelect = $('#sectionMaterials');
+            sectionSelect.empty(); // Clear existing options
+
+            if (sections.length > 0) {
+                sectionSelect.append('<option selected disabled>Section</option>');
+                sections.forEach(section => {
+                    sectionSelect.append('<option value="' + section.section_id + '">' + section.section_name + '</option>');
+                });
+            } else {
+                sectionSelect.append('<option>No sections available</option>');
+            }
+        },
+        error: function() {
+            alert("Error fetching sections.");
+        }
+    });
+}
+
+$(document).ready(function() {
+    loadSections(); // Call loadSections on page load
+});
