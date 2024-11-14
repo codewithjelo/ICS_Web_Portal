1d2931dea85f8ff5771e83c84a58514ca5841712
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-11-14 23:40:28 +0800

    Add ajax

--- partials/head.php
+++ partials/head.php
@@ -28,3 +28,5 @@
 <!-- DataTables JS -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
