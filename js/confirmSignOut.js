0d3295c3004fa511997ae1e8610e6a9293ee9978
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-11-14 23:37:58 +0800

    Modify values

--- js/confirmSignOut.js
+++ js/confirmSignOut.js
@@ -1,16 +1,16 @@
-function confirmLogout() {
+function confirmSignOut() {
     Swal.fire({
         title: 'Are you sure?',
-        text: "Do you really want to logout?",
+        text: "Do you really want to sign out?",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, logout'
+        confirmButtonText: 'Sign Out'
     }).then((result) => {
         if (result.isConfirmed) {
             // Submit the form if confirmed
-            document.getElementById('logoutForm').submit();
+            document.getElementById('signOutForm').submit();
         }
     });
 }
