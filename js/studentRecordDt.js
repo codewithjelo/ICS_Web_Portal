fa12f43157a91adda4b66a9f333c3577e2efffd5
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-11-20 23:22:18 +0800

    Change attributes

--- js/studentRecordDt.js
+++ js/studentRecordDt.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
     $('#studentRecord').DataTable({
-        "paging": true, // Enables pagination
-        "searching": true, // Enables searching
-        "ordering": false, // Enables column sorting
-        "info": true, // Displays information about the table
-        "select": true,
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "lengthChange": false
     });
 });
