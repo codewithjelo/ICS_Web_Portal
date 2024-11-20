f6074f9eaf47ec25f8a1069ac8424f33ec0f8d00
Jelo Dimaano <jannangelodimaano@gmail.com>
2024-11-20 23:12:54 +0800

    Remove echo message

--- function/deleteAnnouncement.php
+++ function/deleteAnnouncement.php
@@ -2,18 +2,13 @@
 include "../connectDb.php";
 
 if (isset($_POST['announcement_id'])) {
-    $announcementId = $_POST['announcement_id'];
+    $announcement_id = $_POST['announcement_id'];
 
     // Delete the announcement from the database
     $query = "DELETE FROM announcements WHERE announcement_id = ?";
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("i", $announcementId);
-
-    if ($stmt->execute()) {
-        echo "Announcement deleted successfully";
-    } else {
-        echo "Error deleting announcement";
-    }
+    $stmt->bind_param("i", $announcement_id);
+    $stmt->execute();
 
     $stmt->close();
     $conn->close();
