9297335aee4f46e4e3c4d24bae68beaee86b480b
Jelo <jannangelodimaano@gmail.com>
2025-12-04 20:30:07 +0800

    Modify mission vision layout

--- pages/content/missionVisionBody.php
+++ pages/content/missionVisionBody.php
@@ -1,45 +1,45 @@
-<div class="mv-scroll rounded-4 p-2" style="height: 520px">
+<div class="mv-scroll rounded-4 p-4" style="height: 520px">
     <!-- Mission and Vision  -->
-    <div class="mission-vision rounded-4 p-4" style="height: 440px;">
-        <div class="overflow-y-scroll" style="height: 385px;">
-            <div class="px-1">
+    <div class="mission-vision overflow-y-scroll position-relative" style="height: 420px;">
 
-                <p class="mission-title fw-bold text-center p-2">MISSION</p>
-                <div class="mission-container">
-                    <p class="mission-text text-break lh-1 rounded-2 p-3">
-                        To protect and promote the right of every Filipino to quality, equitable, culture-based, and complete basic education where:
-                        <br><br><span class="fw-bold">Students</span> learn in a child-friendly, gender-sensitive, safe, and motivating environment.
-                        <br><br><span class="fw-bold">Teachers</span> facilitate learning and constantly nurture every learner.
-                        <br><br><span class="fw-bold">Administrators and staff</span>, as stewards of the institution, ensure an enabling and supportive environment for effective learning to happen.
-                        <br><br><span class="fw-bold">Family, community, and other stakeholders</span> are actively engaged and share responsibility for developing life-long learners.
-                    </p>
-                </div>
-                <p class="vision-title  fw-bold text-center p-2">VISION</p>
-                <div class="vision-container">
-                    <p class="vision-text text-break lh-1 rounded-2 p-3">
-                        We dream of Filipinos
-                        who passionately love their country
-                        and whose values and competencies
-                        enable them to realize their full potential
-                        and contribute meaningfully to building the nation.
-        
-                        As a learner-centered public institution,
-                        the Department of Education
-                        continuously improves itself
-                        to better serve its stakeholders
-                    </p>
-                </div>
+        <div class="px-1">
+
+            <p class="mission-title fw-bold text-center p-2">MISSION</p>
+            <div class="mission-container">
+                <p class="mission-text text-break lh-1 rounded-2 p-3">
+                    To protect and promote the right of every Filipino to quality, equitable, culture-based, and
+                    complete basic education where:
+                    <br><br><span class="fw-bold">Students</span> learn in a child-friendly, gender-sensitive, safe, and
+                    motivating environment.
+                    <br><br><span class="fw-bold">Teachers</span> facilitate learning and constantly nurture every
+                    learner.
+                    <br><br><span class="fw-bold">Administrators and staff</span>, as stewards of the institution,
+                    ensure an enabling and supportive environment for effective learning to happen.
+                    <br><br><span class="fw-bold">Family, community, and other stakeholders</span> are actively engaged
+                    and share responsibility for developing life-long learners.
+                </p>
             </div>
+            <p class="vision-title  fw-bold text-center p-2">VISION</p>
+            <div class="vision-container">
+                <p class="vision-text text-break lh-1 rounded-2 p-3">
+                    We dream of Filipinos
+                    who passionately love their country
+                    and whose values and competencies
+                    enable them to realize their full potential
+                    and contribute meaningfully to building the nation.
 
-
+                    As a learner-centered public institution,
+                    the Department of Education
+                    continuously improves itself
+                    to better serve its stakeholders
+                </p>
+            </div>
         </div>
 
     </div>
-
-
     <div class="text-center mt-4">
-        <a type="button" class="expand text-decoration-none" data-bs-toggle="modal" data-bs-target="#viewMissionVision">Expand</a>
+        <a type="button" class="expand text-decoration-none" data-bs-toggle="modal"
+            data-bs-target="#viewMissionVision">Expand</a>
     </div>
-
 
 </div>
