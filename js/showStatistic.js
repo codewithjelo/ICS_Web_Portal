3c8fbaeebbe6472ffe847f305573d2dc1358d1d6
Jelo <jannangelodimaano@gmail.com>
2025-12-04 20:18:36 +0800

    Refactor file name

--- js/showStatistic.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-    $('#statisticBtn').click(function() {
-        $(this).toggleClass('not-active');
-        $('.main-con').toggleClass('hidden');
-
-        $('#announcement').toggleClass('hidden');
-        $('#missionVision').toggleClass('hidden');
-    });
-});
