7d38b8400a81fcda352c3ead57f821b89ce20538
Jelo <jannangelodimaano@gmail.com>
2025-12-04 20:10:17 +0800

    Refactor variable name

--- function/deleteAnnouncement.php
+++ function/deleteAnnouncement.php
@@ -2,12 +2,12 @@
 include "../connectDb.php";
 
 if (isset($_POST['announcement_id'])) {
-    $announcement_id = $_POST['announcement_id'];
+    $announcementId = $_POST['announcement_id'];
 
 
     $query = "DELETE FROM announcements WHERE announcement_id = ?";
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("i", $announcement_id);
+    $stmt->bind_param("i", $announcementId);
     $stmt->execute();
 
     $stmt->close();
