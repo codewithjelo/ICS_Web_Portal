2328630d797be3e048a0839919d43c24e3b1bff9
Jelo <jannangelodimaano@gmail.com>
2025-12-04 20:22:04 +0800

    Add noopener noreferrer for a tag rel attribute

--- pages/content/footer.php
+++ pages/content/footer.php
@@ -11,9 +11,9 @@
             <div class="col-md-12">
                 <ul class="footer-info d-flex flex-row justify-content-center mt-5 list-inline mx-3" style="column-gap: 200px;">
                     <li class="d-flex flex-row align-items-center"><iconify-icon class="iconify-footer" icon="ri:facebook-circle-line"></iconify-icon><span style="margin: 0 0 0 10px;">
-                        <a href="https://www.facebook.com/DepEdTayoIbaanCS107355" style="text-decoration: none; color: black;">Ibaan Central School</a></span></li>
+                        <a href="https://www.facebook.com/DepEdTayoIbaanCS107355" target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;">Ibaan Central School</a></span></li>
                     <li class="d-flex flex-row align-items-center"><iconify-icon class="iconify-footer" icon="iconamoon:location-pin"></iconify-icon><span style="margin: 0 0 0 10px;">R4FM+5H5, Poblacion Rd, Ibaan, Batangas</span></li>
-                    <li class="d-flex flex-row align-items-center"><iconify-icon class="iconify-footer" icon="ph:phone"></iconify-icon><span style="margin: 0 0 0 10px;">(043) 702 - 3669</span></li>
+                    <li class="d-flex flex-row align-items-center"><iconify-icon class="iconify-footer" icon="ph:phone"></iconify-icon><span style="margin: 0 0 0 10px;"><a href="tel:(043) 702 - 3669" style="text-decoration: none; color: black;">(043) 702 - 3669</a></span></li>
                 </ul>
             </div>
         </div>
