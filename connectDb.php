ee42370d6f79563caf4e0a81583d10a2988fa1a8
Jelo <jannangelodimaano@gmail.com>
2025-12-04 20:03:17 +0800

    Add pdo connection

--- connectDb.php
+++ connectDb.php
@@ -1,11 +1,20 @@
 <?php
-    $host     = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname   ='ics_db';
-    
-    $conn = new mysqli($host, $username, $password, $dbname);
-    if(!$conn){
-        die("Cannot connect to the database.". $conn->error);
-    }
-?>   
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbName = 'ics_db';
+
+// MySQLi connection
+$conn = new mysqli($host, $username, $password, $dbName);
+if (!$conn) {
+    die("Cannot connect to the database." . $conn->error);
+}
+
+// PDO connection
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("PDO connection failed: " . $e->getMessage());
+}
+?>
