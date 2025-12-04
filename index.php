b0e575af2c3cb3ccd1a76a1079c26d1edbd6ba62
Jelo <jannangelodimaano@gmail.com>
2025-12-04 20:04:29 +0800

    Redesign login page and remove forgot password link

--- index.php
+++ index.php
@@ -14,20 +14,24 @@
 
 <body>
     <div class="container d-flex justify-content-center align-items-center min-vh-100">
-        <div class="card shadow-lg rounded-4">
-            <div class="card-header text-center text-white position-relative">
-                <div class="z-2 rounded-top-4 overlay position-absolute top-50 start-50 translate-middle" style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);"></div>
+        <div class="card shadow-lg rounded-4" style="max-width: 620px; width: 100%;">
+            <div class="card-header text-center text-white position-relative" style="padding: 0;">
+                <div class="z-2 rounded-top-4 overlay position-absolute top-0 start-0"
+                    style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);"></div>
                 <img src="img/schoolBanner.jpg" alt="Background" class="card-img-top bg-header img-fluid rounded-top-4">
-                <img src="img/icsLogo.png" alt="Logo" class="z-3 position-absolute top-50 start-50 translate-middle" style="width: 20%;">
+                <img src="img/icsLogo.png" alt="Logo" class="z-3 position-absolute top-50 start-50 translate-middle"
+                    style="width: 15%;">
             </div>
 
-            <div class="card-body rounded-bottom-4 card-body form-body text-white">
+            <div class="card-body rounded-bottom-4 card-body form-body text-white" style="padding: 2rem 1.5rem;">
 
-                <h1 class="text-center mb-4">LOGIN</h1>
+                <h1 class="text-center mb-4" style="font-size: 1.5rem;">LOGIN</h1>
                 <?php if (isset($_SESSION['error_message'])): ?>
-                    <div class="alert alert-danger alert-dismissible fade show d-flex align-items-center" role="alert" style="height: 40px; margin: 0 185px;">
+                    <div class="alert alert-danger alert-dismissible fade show d-flex align-items-center" role="alert"
+                        style="height: 40px; margin: 0 0 1rem 0;">
                         <?php echo $_SESSION['error_message']; ?>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" style="padding: 0.7rem;"></button>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
+                            style="padding: 0.7rem;"></button>
                     </div>
                     <?php unset($_SESSION['error_message']); ?>
                 <?php endif; ?>
@@ -37,7 +41,8 @@
                         <label for="userId" class="form-label">User ID</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-person"></i></span>
-                            <input type="text" class="form-control" id="userId" name="user_id" placeholder="Enter User ID">
+                            <input type="text" class="form-control" id="userId" name="user_id"
+                                placeholder="Enter User ID">
                         </div>
                     </div>
 
@@ -45,17 +50,20 @@
                         <label for="password" class="form-label">Password</label>
                         <div class="input-group">
                             <span class="input-group-text"><i class="bi bi-lock"></i></span>
-                            <input type="password" class="form-control border-end-0" id="password" placeholder="Enter Password" name="user_password" style="padding-right: 10px;">
+                            <input type="password" class="form-control border-end-0" id="password"
+                                placeholder="Enter Password" name="user_password" style="padding-right: 10px;">
                             <span class="input-group-text border-0" style="width: 35px; background-color: white;">
-                                <i class="position-absolute top-50 end-0 translate-middle-y bi bi-eye-slash" style="color: black; padding-right: 10px;" id="togglePasswordIcon" onclick="togglePassword()"></i>
+                                <i class="position-absolute top-50 end-0 translate-middle-y bi bi-eye-slash"
+                                    style="color: black; padding-right: 10px;" id="togglePasswordIcon"
+                                    onclick="togglePassword()"></i>
                             </span>
                         </div>
                         <small class="form-text text-light">* Password is case sensitive</small>
-                        <a href="forgotPassword" class="text-light d-block mt-2">Forgot Password?</a>
                     </div>
 
                     <div class="d-flex justify-content-center">
-                        <button type="submit" class="btn btn-warning rounded-5"><i class="bi bi-box-arrow-in-right"></i> Sign In</button>
+                        <button type="submit" class="btn btn-warning rounded-5"><i class="bi bi-box-arrow-in-right"></i>
+                            Sign In</button>
                     </div>
                 </form>
             </div>
